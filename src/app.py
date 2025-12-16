0ed0b0880a534a451a4e0476e9116941e9e380fa
guilhermesolutions <guilhermesolutionsgroup@gmail.com>
2025-12-16 11:36:31 -0500

    Create Flask app with greet_user route
    Added a Flask application with a greet_user route that renders user input in HTML.

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template_string
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/greet', methods=['GET'])
+def greet_user():
+    # Accepting user input directly from query parameters
+    user_name = request.args.get('name')
+
+    # Vulnerable to XSS, as input is directly rendered in HTML without sanitization
+    html_content = f'''
+    <html>
+        <body>
+            <h1>Hello, {user_name}!</h1>
+        </body>
+    </html>
+    '''
+    return render_template_string(html_content)
+
+if __name__ == '__main__':
+    app.run(debug=True)
